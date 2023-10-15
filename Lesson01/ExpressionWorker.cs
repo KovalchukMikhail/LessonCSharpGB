9683bae586d0efae5ff5f92f958ad5adee950b38
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2023-10-15 17:38:26 +0300

    HomeWork one added

---
+++ Lesson01/ExpressionWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01
+{
+    public class ExpressionWorker
+    {
+        public string GetResultExpression(string argOne, string argTwo, string action)
+        {
+            return ExecuteExpression(Convert.ToDouble(argOne), Convert.ToDouble(argTwo), action).ToString();
+        }
+        public double ExecuteExpression(double argOne, double argTwo, string action) => action switch
+        {
+            "*" => argOne * argTwo,
+            "/" => argOne / argTwo,
+            "+" => argOne + argTwo,
+            "-" => argOne - argTwo,
+            _ => throw new ArgumentException($"Получена некорректная операци для вычесления{action}"),
+        };
+    }
+}
--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Lesson01
+﻿using System;
+
+namespace Lesson01
 {
+    //Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a* b, введенные из командной строки, и выводящую результат выполнения на экран.
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Не были переданны параметры");
+                    return;
+                }
+                ExpressionWorker executer = new ExpressionWorker();
+                List<string> expression = args.ToList();
+                if (expression[0] == "-")
+                {
+                    expression.RemoveAt(0);
+                    expression[0] = "-" + expression[0];
+                }
+                while (expression.Count > 2)
+                {
+                    for(int i = 1; i < expression.Count - 1; i++)
+                    {
+                        if (expression[i] == "*" || expression[i] == "/")
+                        {
+                            expression[i - 1] = executer.GetResultExpression(expression[i - 1], expression[i + 1], expression[i]);
+                            expression.RemoveRange(i, 2);
+                            break;
+                        }
+                    }
+                    for(int i = 1; i < expression.Count - 1; i++)
+                    {
+                        if (expression[i] == "-" || expression[i] == "+")
+                        {
+                            expression[i - 1] = executer.GetResultExpression(expression[i - 1], expression[i + 1], expression[i]);
+                            expression.RemoveRange(i, 2);
+                            i = i - 1;
+                        }
+                    }
+                }
+                Console.WriteLine($"Результат выражения: {Convert.ToDouble(expression[0])}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Переданно некоректное вырожение. Получно исключение: {ex}");
+            }
         }
+
     }
 }
