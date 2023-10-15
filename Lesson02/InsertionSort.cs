ef99331ec65214cc2b0efce440b0dacf615e77a3
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2023-10-15 17:38:39 +0300

    HomeWork two added

---
+++ Lesson02/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    public static class InsertionSort
+    {
+        public static void AddOnRightPlace(this List<int> numbers, int nextNum)
+        {
+            numbers.Add(nextNum);
+            int temp = nextNum;
+            int i = numbers.Count - 1;
+            while (i > 0 && numbers[i - 1] > temp)
+            {
+                numbers[i] = numbers[i - 1];
+                i--;
+            }
+            numbers[i] = temp;
+        }
+    }
+}
---
+++ Lesson02/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Lesson02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            int sizeOne = a.GetLength(0);
+            int sizeTwo = a.GetLength(1);
+            List<int> temp = new List<int>();
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < sizeOne; i++)
+            {
+                for(int j = 0; j < sizeTwo; j++)
+                {
+                    temp.AddOnRightPlace(a[i, j]);
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int k = 0;
+            Console.WriteLine("Отсортированный массив:");
+            for (int i = 0; i < sizeOne; i++)
+            {
+                for (int j = 0; j < sizeTwo; j++)
+                {
+                    a[i, j] = temp[k++];
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
